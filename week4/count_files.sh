a832b6324edffc5101b3941609d0d5535db5b442
Michael Smith <x00107586@gmail.com>
2016-10-14 10:52:45 +0100

    changed

--- week4/count_files.sh
+++ week4/count_files.sh
@@ -3,7 +3,9 @@
 # Week 4
 # Scan a directory and count the number of files it finds in the directory
 
+# Change directory to the directory mentioned first
 cd $1
 
-ls -1 | wc -l
+
+ls $1 | wc -l
 
--- week4/pipe.sh
+++ week4/pipe.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Michael Smith
 # Week 4
+# Pipe can be used to put multiple commands together to minimize a search result
+# A pipe is a form of redirection that is used in Linux OS to send the output of one program to another program for further processing.
+
 
 # Gives a full list of processes of my sql
+# ps -ef displays info on all running processes
+# grep mysql searches for the word mysql
+# Together this command lists all processes and searches for the processes containing mysql
 ps -ef | grep mysql
